2323c06f5fca1bf8856a4f6184b7cc9127b2bee9
oscar <oscarollie366@gmail.com>
2026-02-02 14:45:49 +0300

    Fix ThemeContext import

--- frontend/src/components/DarkToggle.jsx
+++ frontend/src/components/DarkToggle.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
+import { ThemeContext } from "../context/ThemeContext.js";
 
 export default function DarkToggle() {
   const { dark, setDark } = useContext(ThemeContext);
