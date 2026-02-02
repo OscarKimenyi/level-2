1b2c69215c4a5ae6f218f191a1a3644380029c5d
oscar <oscarollie366@gmail.com>
2026-02-02 15:58:28 +0300

    Add Profile route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
 function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
@@ -25,6 +26,7 @@ function App() {
             </ProtectedRoute>
           }
         />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
