6b0e0525fd0c488509750c91df7188aa8a3a5383
oscar <oscarollie366@gmail.com>
2026-02-02 14:31:40 +0300

    Create ThemeProvider.jsx

---
+++ frontend/src/context/ThemeProvider.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+export default function ThemeProvider({ children }) {
+  const [dark, setDark] = useState(false);
+
+  return (
+    <ThemeContext.Provider value={{ dark, setDark }}>
+      <div
+        className={
+          dark
+            ? "bg-dark text-light min-vh-100"
+            : "bg-light text-dark min-vh-100"
+        }
+      >
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  );
+}
