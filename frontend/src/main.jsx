2d5e7b40f94c91013271e44b878713b779c1668c
oscar <oscarollie366@gmail.com>
2026-02-02 16:31:36 +0300

    Import Footer to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import ThemeProvider from "./context/ThemeProvider.jsx";
 import "./index.css";
+import Footer from "./components/Footer";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Footer />
     </ThemeProvider>
   </StrictMode>,
 );
