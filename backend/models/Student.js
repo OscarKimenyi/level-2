85b169a34773b6cdadb8c9d253281bebf934b77d
oscar <oscarollie366@gmail.com>
2026-02-02 19:38:36 +0300

    Add photo field Student.js model

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -4,6 +4,7 @@ const StudentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   regNo: { type: String, required: true, unique: true },
   course: { type: String, required: true },
+  photo: { type: String },
 });
 
 module.exports = mongoose.model("Student", StudentSchema);
