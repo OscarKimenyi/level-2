59a735a338fc6f3a2fbd3b357b56357486cb50fb
oscar <oscarollie366@gmail.com>
2026-02-02 12:30:42 +0300

    Update App.jsx to protect Dash board page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,28 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
+const Protected = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Login />;
+};
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Protected>
+              <Dashboard />
+            </Protected>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
