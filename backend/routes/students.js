3c72589f5ea1d20e990c3b08e0dbffa9f5fc2df3
oscar <oscarollie366@gmail.com>
2026-02-02 13:28:09 +0300

    Add Search and Pagination to students.js

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -3,25 +3,39 @@ const Student = require("../models/Student");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 
-// Get All
+// GET WITH SEARCH + PAGINATION
 router.get("/", auth, async (req, res) => {
-  res.json(await Student.find());
+  const page = parseInt(req.query.page) || 1;
+  const limit = 5;
+  const search = req.query.search || "";
+
+  const query = {
+    name: { $regex: search, $options: "i" },
+  };
+
+  const students = await Student.find(query)
+    .skip((page - 1) * limit)
+    .limit(limit);
+
+  const total = await Student.countDocuments(query);
+
+  res.json({ students, totalPages: Math.ceil(total / limit) });
 });
 
-// Add
+// ADD
 router.post("/", auth, async (req, res) => {
   const student = new Student(req.body);
   await student.save();
   res.json(student);
 });
 
-// Update
+// UPDATE
 router.put("/:id", auth, async (req, res) => {
   await Student.findByIdAndUpdate(req.params.id, req.body);
   res.json("Updated");
 });
 
-// Delete (ADMIN ONLY)
+// DELETE (ADMIN)
 router.delete("/:id", admin, async (req, res) => {
   await Student.findByIdAndDelete(req.params.id);
   res.json("Deleted");
